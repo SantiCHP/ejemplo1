1da26043e9f1823db9c35e96effad2c7778d2c6f
Santiago Castellano Hernández <santichadmin@gmail.com>
2020-11-17 15:36:27 +0000

    Actualizo ejemplo con los avances

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ejemplo1';
+  title = 'Hola mundo';
+  color:String = 'red';
 }
